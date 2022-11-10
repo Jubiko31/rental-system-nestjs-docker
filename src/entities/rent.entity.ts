8c0d83fcecb9574d1fbe466123fdd738204422d0
Jubiko31 <jubiko.macaberidze1@gmail.com>
2022-11-10 16:24:10 +0400

    make new lease && calculation of rental price

--- src/entities/rent.entity.ts
+++ src/entities/rent.entity.ts
@@ -1,7 +1,14 @@
 export interface Rent {
   id: number;
-  startDate: Date;
-  lastDate: Date;
+  startdate: Date;
+  lastdate: Date;
   rent_price: number;
   car_id: number;
+}
+
+export interface Contract {
+  Response: string;
+  Car_ID?: number;
+  Rent_Period?: string;
+  Total_Price?: string;
 }
